f07b275d88a22649627fbfc683a6f26a3b412a37
e5f6bd <41416412+e5f6bd@users.noreply.github.com>
2020-04-18 12:25:51 +0900

    Refresh the tokens when expired.
    This refreshes both access token and refresh token, though the latter is not verified to be actually working.

--- src/drive.ts
+++ src/drive.ts
@@ -8,17 +8,23 @@ export const createNewOauthClient = () => new google.auth.OAuth2(
     "urn:ietf:wg:oauth:2.0:oob",
 );
 
+const TOKENS_PATH = "./tokens.json";
+
 export const createDriveClient = async (): Promise<Drive> => {
     const oauth2Client = createNewOauthClient();
-    oauth2Client.setCredentials(JSON.parse(await fs.promises.readFile("./tokens.json", "utf-8")));
-    oauth2Client.on('tokens', (tokens) => {
+    oauth2Client.setCredentials(JSON.parse(await fs.promises.readFile(TOKENS_PATH, "utf-8")));
+    oauth2Client.on('tokens', async (tokens) => {
         // if (tokens.refresh_token) {
         //     // store the refresh_token in my database!
         //     console.log(tokens.refresh_token);
         // }
         // console.log(tokens.access_token);
-        console.log("Tokens has been refreshed:");
-        console.log(JSON.stringify(tokens));
+        // console.log("Tokens has been refreshed:");
+        // console.log(JSON.stringify(tokens));
+
+        const oldTokens = JSON.parse(await fs.promises.readFile(TOKENS_PATH, "utf-8"));
+        const newTokens = Object.assign({}, oldTokens, tokens);
+        await fs.promises.writeFile(TOKENS_PATH, JSON.stringify(newTokens));
     });
 
     return google.drive({version: "v3", auth: oauth2Client});
